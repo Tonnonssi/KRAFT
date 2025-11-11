2e5151c992f34743ee5c356c0acee9e0d33f8a4c
Jimin <tonnossi@gmail.com>
2025-11-11 17:50:11 +0900

    [feat]single, multi agent를 케이스 별로 처리,

--- kraft/runner/core/running.py
+++ kraft/runner/core/running.py
@@ -1,13 +1,23 @@
 import torch
 import numpy as np
 
-def run_episode(env, agent, n_steps, device, callbacks=None):
+def run_episode(env, agent, n_steps, device, callbacks=None, multi_critics=False):
     """
     하나의 에피소드를 실행한다. 
     [n_steps]이란? 
     - PPO train일 때는 on-policy 학습을 위한 값 
     - PPO가 아니거나, valid, test 때는 상한 스텝 수로 해석
+    ----------------------------------------------------------------------
+    single critic과 multi critics에 따라 다른 실행 함수를 호출한다. 
+    
     """
+    if multi_critics: 
+        return _run_episode_multi_critics(env, agent, n_steps, device, callbacks)
+    else:
+        return _run_episode_single_critic(env, agent, n_steps, device, callbacks)
+
+def _run_episode_single_critic(env, agent, n_steps, device, callbacks=None):
+    """single critic PPO 에이전트인 경우"""
     done = False 
     state = env.reset()
     state = get_trainable_state(state, device)
@@ -24,13 +34,15 @@ def run_episode(env, agent, n_steps, device, callbacks=None):
         next_state, reward, done, mask = env.step(decoded_action)
 
         next_state = get_trainable_state(next_state, device)
-        step_memory = get_memory(state, decoded_action, reward, next_state, done, log_prob, mask, env.is_entry, env.log_return)
-        step_extra_memory = get_extra_memory(env, decoded_action)
+        step_memory = build_memory(state, decoded_action,                   # on-policy를 위한 메모리 
+                                   reward, next_state, done, log_prob,      
+                                   mask, env.is_entry, env.log_return)
+        step_extra_memory = get_extra_memory(env, decoded_action)           # 시각화 등을 위한 추가 메모리
         epi_memory.append((step_memory, step_extra_memory))
 
         # UPDATE 
         state = next_state
-        epi_reward += reward
+        epi_reward += reward 
 
         if callbacks:
             for cb in callbacks:
@@ -38,7 +50,60 @@ def run_episode(env, agent, n_steps, device, callbacks=None):
 
     return epi_memory, epi_reward, env.episode_event
     
-def run_loop(env, agent, batch_size, n_steps, is_training: bool, device, callbacks=None):
+def _run_episode_multi_critics(env, agent, n_steps, device, callbacks=None):
+    """
+    multi critics PPO 에이전트인 경우
+    """
+    def weighted_reward(reward, alpha):
+        """multi critics PPO 에이전트에서 사용하는 가중 보상 계산"""
+        reward_tensor = torch.tensor(reward, dtype=torch.float32)
+        alpha_tensor = torch.tensor(alpha, dtype=torch.float32)
+        weighted = torch.sum(reward_tensor * alpha_tensor).clone().detach().item()
+        return weighted
+    
+    done = False 
+    state = env.reset()
+    state = get_trainable_state(state, device)
+    mask = env.mask
+
+    # Multi-Critics PPO 에이전트인 경우, 환경에서 alpha 값을 받아 에이전트에 설정
+    # ===============================================================
+    env.reset_alpha()
+    agent.get_alpha(env.alpha)
+    # ===============================================================
+
+    epi_memory = []
+    epi_reward = 0
+
+    for idx in range(n_steps):
+        if done:
+            break 
+
+        decoded_action, log_prob = agent.get_action(state, mask, stochastic=True)
+        next_state, reward, done, mask = env.step(decoded_action)
+
+        next_state = get_trainable_state(next_state, device)
+        # ===============================================================
+        # single critic과 다른 부분: alpha 값을 메모리에 추가
+        step_memory = build_memory(state, decoded_action,                   # on-policy를 위한 메모리 
+                                   reward, next_state, done, log_prob,      
+                                   mask, env.is_entry, env.log_return, 
+                                   env.alpha)
+        # ===============================================================
+        step_extra_memory = get_extra_memory(env, decoded_action)           # 시각화 등을 위한 추가 메모리
+        epi_memory.append((step_memory, step_extra_memory))
+
+        # UPDATE 
+        state = next_state
+        epi_reward += weighted_reward(reward, env.alpha)
+
+        if callbacks:
+            for cb in callbacks:
+                cb.on_step_end(get_step_log(env, decoded_action, reward, idx))
+
+    return epi_memory, epi_reward, env.episode_event
+
+def run_loop(env, agent, batch_size, n_steps, is_training: bool, device, callbacks=None, multi_critics=False):
     """
     Train, Valid, Test의 공통 로직을 처리하는 핵심 루프.
     is_training 플래그에 따라 학습 관련 동작을 제어한다.
@@ -54,7 +119,7 @@ def run_loop(env, agent, batch_size, n_steps, is_training: bool, device, callbac
     while not env.terminated:
         # 1. 에피소드 실행 (공통)
         # is_training 플래그를 전달하여 에이전트가 탐험(train) 또는 결정적 행동(valid/test)을 하도록 제어할 수 있음
-        epi_memory, epi_reward, episode_event = run_episode(env, agent, n_steps, device, callbacks)
+        epi_memory, epi_reward, episode_event = run_episode(env, agent, n_steps, device, callbacks, multi_critics)
         _episode_cum_reward += epi_reward
 
         # 2. 학습 로직 (is_training=True일 때만 실행)
@@ -172,10 +237,11 @@ def get_trainable_state(state, device):
     agent_state = torch.tensor(state[1], dtype=torch.float32).unsqueeze(0).to(device)
     return ts_state, agent_state
 
-def get_memory(state, decoded_action, reward, next_state, done, log_prob, mask, is_entry, log_return):
-    return [
+def build_memory(state, decoded_action, reward, next_state, done, log_prob, mask, is_entry, log_return, alpha=None):
+    '''memory를 구성하여 반환'''
+    mem =  [
             state,
-            torch.tensor([[decoded_action]]),
+            torch.tensor([decoded_action]),
             torch.tensor([reward], dtype=torch.float32),
             next_state,
             torch.tensor([done], dtype=torch.float32),
@@ -184,6 +250,11 @@ def get_memory(state, decoded_action, reward, next_state, done, log_prob, mask, 
             torch.tensor([is_entry], dtype=torch.bool),
             torch.tensor([log_return], dtype=torch.float32),
             ]
+    # alpha 값이 존재하는 경우 메모리에 추가
+    if alpha is not None:
+        mem.append(alpha.clone().unsqueeze(0))
+
+    return mem
 
 def split_position_strength(decoded_action):
     if decoded_action == 0:
